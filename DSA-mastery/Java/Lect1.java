3af07f499aeb1566abc232a5c39dc55b90373d57
Reshika <riya.kumari6966@gmail.com>
2026-06-29 11:19:44 +0530

    lecture 1 java

---
+++ DSA-mastery/Java/Lect1.java
@@ -0,0 +1,9 @@
+package Java;
+
+public class Lect1 {
+     public static void main(String[] args){
+        System.out.print("Hello World\n");
+        System.out.print("Hello World\n");
+        System.out.print("*\n**\n***\n****");
+     }
+}
---
+++ DSA-mastery/Java/Lect2.java
@@ -0,0 +1,8 @@
+package Java;
+
+public class Lect2 {
+    public static void main(String[] args){
+        
+    }
+    
+}
