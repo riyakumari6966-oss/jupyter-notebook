76777974912cff154dd251ec393b28911163356f
Reshika <riya.kumari6966@gmail.com>
2026-06-30 11:00:33 +0530

    for loop

---
+++ DSA-mastery/Java/Loops.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class Loops {
+    public static void main(String args[]){
+        //i++ => i=i+1
+        //for(initialization;condition;updation){ to do something }
+        for (int i=0; i<11; i++){
+            System.out.print(i+" ");
+        }
+    }
+}
