3df25249d5776b663ef4fbe877233a9994ade9ac
Reshika <riya.kumari6966@gmail.com>
2026-06-30 10:28:44 +0530

    lect 3 ex 3

---
+++ DSA-mastery/Java/conditional1.java
@@ -0,0 +1,21 @@
+package Java;
+import java.util.*;
+public class conditional1 {
+    public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      int b= sc.nextInt();
+      if (a==b){
+        System.out.println("equal");
+      } 
+      else if(a>b){
+            System.out.println("a is greater than b");
+        }
+      else{
+        System.out.println("b is greater than a");
+      }
+      
+
+    }
+    
+}
